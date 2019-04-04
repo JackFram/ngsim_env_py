d4131e0aa1260861f0bcc0511ef91428492507f1
JackFram <erdos_zzh@163.com>
2019-04-04 23:38:25 +0100

    Env.py added

---
+++ env.py
@@ -0,0 +1,31 @@
+# Zhihao Zhang
+# ngsim_env_py Env class
+
+
+import DeterministicSingleStepDebugEnv, NGSIMEnv, VectorizedNGSIMEnv, MultiagentNGSIMEnv, MultiagentNGSIMEnvVideoMaker
+
+
+class Env:
+
+    def __init__(self, env_id: str, env_params: dict):
+        self.env_id = env_id
+        self.env_params = env_params
+
+    def make_env(self):
+        try:
+            if self.env_id == "DeterministicSingleStepDebugEnv":
+                return DeterministicSingleStepDebugEnv(self.env_params)
+            elif self.env_id == "NGSIMEnv":
+                return NGSIMEnv(self.env_params)
+            elif self.env_id == "VectorizedNGSIMEnv":
+                return VectorizedNGSIMEnv(self.env_params)
+            elif self.env_id == "MultiagentNGSIMEnv":
+                return MultiagentNGSIMEnv(self.env_params)
+            elif self.env_id == "MultiagentNGSIMEnvVideoMaker":
+                return MultiagentNGSIMEnvVideoMaker(self.env_params)
+            else:
+                raise ValueError("We don't have a env with this name, try another one!")
+        except Exception as error:
+            print('Caught this error: ' + repr(error))
+
+
